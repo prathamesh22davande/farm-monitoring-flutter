9c9229215882401221f14fa7ebb2ef97275211d0
prathamesh22davande <prathamesh22davande@gmail.com>
2021-05-02 20:41:16 +0530

    User can now view data corresponding to him

--- lib/screens/CalenderScreen.dart
+++ lib/screens/CalenderScreen.dart
@@ -4,6 +4,7 @@ import 'package:farm_monitoring_flutter/models/Task.dart';
 import 'package:http/http.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 
+import 'week_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
@@ -20,7 +21,6 @@ class _CalenderState extends State<Calender> {
   Map<DateTime, List<dynamic>> _events;
   Map<DateTime, List<dynamic>>  _activity;
   List<dynamic> _selectedEvents;
-  DateTime selectedDate;
   TextEditingController _eventController;
   SharedPreferences prefs;
   bool flag = false;
@@ -41,83 +41,85 @@ class _CalenderState extends State<Calender> {
     _events = {};
     _activity={};
     _selectedEvents = [];
-    //prefsData();
+    prefsData();
   }
-  // prefsData() async {
-  //   prefs = await SharedPreferences.getInstance();
-  //   setState(() {
-  //     _events = Map<DateTime, List<dynamic>>.from(
-  //         decodeMap(json.decode(prefs.getString("events") ?? "{}")));
-  //   });
-  // }
+
+  prefsData() async {
+    prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _events = Map<DateTime, List<dynamic>>.from(
+          decodeMap(json.decode(prefs.getString("events") ?? "{}")));
+    });
+  }
 
 
-  // Map<String, dynamic> encodeMap(Map<DateTime, dynamic> map) {
-  //   Map<String, dynamic> newMap = {};
-  //   map.forEach((key, value) {
-  //     newMap[key.toString()] = map[key];
-  //   });
-  //   print(newMap);
-  //   return newMap;
-  // }
-  //
-  // Map<DateTime, dynamic> decodeMap(Map<String, dynamic> map) {
-  //   Map<DateTime, dynamic> newMap = {};
-  //   map.forEach((key, value) {
-  //     newMap[DateTime.parse(key)] = map[key];
-  //   });
-  //   print(newMap);
-  //   return newMap;
-  // }
+  Map<String, dynamic> encodeMap(Map<DateTime, dynamic> map) {
+    Map<String, dynamic> newMap = {};
+    map.forEach((key, value) {
+      newMap[key.toString()] = map[key];
+    });
+    print(newMap);
+    return newMap;
+  }
+
+  Map<DateTime, dynamic> decodeMap(Map<String, dynamic> map) {
+    Map<DateTime, dynamic> newMap = {};
+    map.forEach((key, value) {
+      newMap[DateTime.parse(key)] = map[key];
+    });
+    print(newMap);
+    return newMap;
+  }
+
   TextStyle dayStyle(FontWeight fontWeight, Color color) {
     return TextStyle(color: color, fontWeight: fontWeight);
   }
-  // Container taskList(
-  //     String task, String description, IconData img, Color color) {
-  //   return Container(
-  //     padding: EdgeInsets.only(top: 20),
-  //     child: Row(
-  //       children: [
-  //         Icon(
-  //           img,
-  //           color: color,
-  //           size: 20,
-  //         ),
-  //         Container(
-  //           padding: EdgeInsets.only(left: 10, right: 10),
-  //           width: MediaQuery.of(context).size.width * 0.8,
-  //           child: Column(
-  //             crossAxisAlignment: CrossAxisAlignment.start,
-  //             children: [
-  //               Text(
-  //                 task,
-  //                 style: TextStyle(
-  //                     color: Colors.white,
-  //                     fontWeight: FontWeight.bold,
-  //                     fontSize: 18),
-  //               ),
-  //               SizedBox(
-  //                 height: 5.0,
-  //               ),
-  //               Text(
-  //                 description,
-  //                 style: TextStyle(
-  //                     fontWeight: FontWeight.normal,
-  //                     fontSize: 15.0,
-  //                     color: Colors.white),
-  //               )
-  //             ],
-  //           ),
-  //         )
-  //       ],
-  //     ),
-  //   );
-  // }
+
+  Container taskList(
+      String task, String description, IconData img, Color color) {
+    return Container(
+      padding: EdgeInsets.only(top: 20),
+      child: Row(
+        children: [
+          Icon(
+            img,
+            color: color,
+            size: 20,
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 10, right: 10),
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  task,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Text(
+                  description,
+                  style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 15.0,
+                      color: Colors.white),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomPadding: false,
       body: SingleChildScrollView(
         child: Container(
           padding: EdgeInsets.all(10.0),
@@ -162,10 +164,12 @@ class _CalenderState extends State<Calender> {
                 onDaySelected: (date, events, holidays) {
                   setState(() {
                     _selectedEvents = events;
-                    loadTasks();
                   });
-                  flag=true;
-                  selectedDate=date;
+                },
+                onDayLongPressed: (date, events, hoildays) {
+                  // var events=encodeMap(_events,);
+                  // listOfEvents=encode(events);
+                  flag = true;
                 },
                 builders: CalendarBuilders(
                   selectedDayBuilder: (context, date, events) => Container(
@@ -190,9 +194,65 @@ class _CalenderState extends State<Calender> {
                       )),
                 ),
               ),
-              SizedBox(child: Container(color:Colors.black),),
-              Container(
-                child:flag?_buildListView(tasks):Text('task'),
+              SizedBox(height: 30.0),
+              SingleChildScrollView(
+                child: flag == true
+                    ? WeekViewDisplay(event: _events,activity: _activity)
+                    : Container(
+                  padding: EdgeInsets.only(left: 30.0),
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height - 215,
+                  decoration: BoxDecoration(
+                      color: Color(0xff00A961),
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30.0),
+                          topRight: Radius.circular(30.0))),
+                  child: SingleChildScrollView(
+                    child: Stack(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.only(top: 50),
+                              child: Text(
+                                "Tasks",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 30.0),
+                              ),
+                            ),
+                            ..._selectedEvents.map((event) => Container(
+                              padding: EdgeInsets.only(
+                                  top: 15.0, left: 20.0),
+                              child: Row(
+                                  crossAxisAlignment:
+                                  CrossAxisAlignment.start,
+                                  children: [
+                                    Icon(
+                                      Icons.chevron_right,
+                                      color: Colors.white,
+                                    ),
+                                    Column(
+                                      children: [
+                                        Container(
+                                          child:
+                                          TextDisplay(event: event),
+                                        ),
+                                        Divider(
+                                          color: Colors.black,
+                                        )
+                                      ],
+                                    ),
+                                  ]),
+                            )),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
@@ -209,8 +269,10 @@ class _CalenderState extends State<Calender> {
           _showAddDialog(context);
         },
       ),
+      resizeToAvoidBottomPadding: false,
     );
   }
+
   _showAddDialog(BuildContext context) async {
     await showDialog(
         context: context,
@@ -293,64 +355,6 @@ class _CalenderState extends State<Calender> {
         ));
   }
 
-  _buildListView(List<Task> tasks) {
-    List<Task> taskPrint=[];
-    for(var task in tasks)
-      {
-        if(selectedDate==task.date)
-          {
-            taskPrint.add(task);
-          }
-      }
-    return SingleChildScrollView(
-      physics: ScrollPhysics(),
-      child: ListView.builder(
-        physics: NeverScrollableScrollPhysics(),
-        scrollDirection: Axis.vertical,
-        shrinkWrap: true,
-        itemCount: taskPrint.length,
-        itemBuilder: (context, index) {
-          return Column(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                    color: Color(0xff00A961),
-                    borderRadius: BorderRadius.circular(10.0)
-                ),
-                child: ListTile(
-                  title: Text('Task', style: TextStyle(color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0)),
-                  subtitle: Container(
-                    padding: EdgeInsets.all(15.0),
-                    child: Column(
-                      children: [
-                        Text(
-                          taskPrint[index].taskName,
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20.0),
-                        ),
-                        Text('Description:'),
-                        Text(taskPrint[index].taskDescription.toString(),
-                          style: TextStyle(
-                              color: Colors.white, fontSize: 15.0),),
-                        Text(taskPrint[index].date.toString(), style: TextStyle(
-                            color: Colors.white),),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 10.0,)
-            ],
-          );
-        },
-      ),
-    );
-  }
-
   loadTasks() async{
     List<Task> arr = await getTasks();
     setState(() {
@@ -388,8 +392,29 @@ class _CalenderState extends State<Calender> {
 
   }
 
+
+
 }
 
-
+class TextDisplay extends StatelessWidget {
+  var event;
+  TextDisplay({this.event});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints(
+        maxWidth: 250.0,
+        minWidth: 50.0,
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 6.0),
+      child: Text(
+        event,
+        softWrap: true,
+        style: TextStyle(
+            color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
+      ),
+    );
+  }
+}
 
 
